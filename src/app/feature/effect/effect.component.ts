724de326ca21aa1e046e0f731605b29cc8d5cfda
rushikesh-salunke <rushikesh.salunke@mindbowser.com>
2023-09-20 18:19:57 +0530

    effect funtionality

--- src/app/feature/effect/effect.component.ts
+++ src/app/feature/effect/effect.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-effect',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./effect.component.scss']
 })
 export class EffectComponent {
+  product = signal({
+    name: 'Laptop',
+    category: 'Electronics',
+    price: 2000,
+    rating: 4.5,
+  });
 
+  changeProductName(){
+    this.product.mutate(product => product.name = 'Mobile');
+  }
+
+  sideEffect = effect(()=> this.saveProductDatatoAPI(this.product()))
+
+  saveProductDatatoAPI(product:any){
+    console.log('Call Api with product Data: ',product);
+  }
 }
